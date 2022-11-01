5f88d635c26d12a98d4ffda702785b576c2fe060
Tagbo Chinedu <johntagbo2@gmail.com>
2022-11-01 23:47:11 +0100

    redux project

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
-export const store =configureStore({
-    reducer:{}
-})
+export const store = configureStore({
+  reducer: { counter: counterReducer },
+});
